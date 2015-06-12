ac6cf11f33cb021be0731794a1440f532e0d9612
Shaff <saiferdima@gmail.com>
2015-06-13 01:27:49 +0300

    Implement methods  Display Title  Find a word  List of all Urls
    Update main method

--- src/com/sourceit/java/basic/Popazovdk/Leasson19/Lab/WGet.java
+++ src/com/sourceit/java/basic/Popazovdk/Leasson19/Lab/WGet.java
@@ -9,96 +9,180 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class WGet {
-static InputStream is = null;
-static BufferedReader br;
-public static String stringUrl = "No Url";
+    static InputStream is = null;
+    static BufferedReader br;
+    public static String stringUrl = "No Url";
 
-public static final String[] COMMANDS = {"Save page", "Display title", "Find a word", "List all URL's on the page", "Exit programm"};
-public static final String FILE_NAME= "C://Users/Shaff/git/sourceIt/src/com/sourceit/java/basic/Popazovdk/Leasson19/WebPage.html";	
-	public static void main(String[] args) throws MalformedURLException {
+    public static final String[] COMMANDS = {"Save page", "Display title", "Find a word", "List all URL's on the page (Betahttp://vk.com/)", "Exit programm"};
+    public static final String FILE_NAME = "D://DL/WebPage.html";
 
-		if(args.length == 0){
-			System.out.println(getInstructions());
-			return;
-		}
-		
-		String command = args[0];
+    public static void main(String[] args) throws MalformedURLException {
 
-		if("-i".equals(command)){
-			//TODO ask for URL
-			System.out.println("Enter an URL:");
-		} else if(!command.startsWith("http")){
-			System.out.println(getInstructions());
-			return;
-		}
-		
-		//TODO list of options
-		stringUrl =args[0];
-		System.out.println("Select what to do next:");
-		
-		for(int i = 0; i < COMMANDS.length; i++){
-			System.out.println((i+1) + ") " + COMMANDS[i]);
-		}
-		
-		int choise = -1;
-		//TODO ask for input
-		 Scanner in = new Scanner(System.in);
-		try{
-			 choise = in.nextInt();
-		}
-		catch (InputMismatchException e){
+        if (args.length == 0) {
+            System.out.println(getInstructions());
+            return;
+        }
+
+        String command = args[0];
+
+        if ("-i".equals(command)) {
+
+            System.out.println("Enter an URL:");
+            Scanner in = new Scanner(System.in);
+            try {
+                command = in.nextLine();
+
+
+            } catch (InputMismatchException e) {
+                System.out.print("Incorrect value.");
+                return;
+            }
+            if (!command.startsWith("http")) {
+                System.out.println("Entered url is not valid");
+                return;
+            }
+            args[0] = command;
+
+        } else if (!command.startsWith("http")) {
+            System.out.println(getInstructions());
+            return;
+        }
+
+        //TODO list of options
+        stringUrl = args[0];
+        System.out.println("Select what to do next:");
+
+        for (int i = 0; i < COMMANDS.length; i++) {
+            System.out.println((i + 1) + ") " + COMMANDS[i]);
+        }
+
+        int choise = -1;
+        //TODO ask for input
+        Scanner in = new Scanner(System.in);
+        try {
+            choise = in.nextInt();
+        } catch (InputMismatchException e) {
             System.out.print("Incorrect value.");
             return;
-			
-		}
-       
-		executeCommand(choise);
-	}
-	
-	private static void executeCommand(int commandNumber){
-		switch (commandNumber) {
-		case 1:
-			System.out.println("Page will be saved to file: \n"+FILE_NAME);
-			savePageToFile(stringUrl,FILE_NAME);
-			break;
 
-		case 2:
-			//TODO
-			break;
-		case 3:
-			//TODO
-			break;
-		case 4:
-			//TODO
-			break;
-		default:
-			
-			break;
-		}
-	}
-	
-	public static void savePageToFile(String StringUrl, String fileName){
-		String line;
-		StringBuffer sb = new StringBuffer();
-		try {
-			URL url = new URL(StringUrl);
-			 is = url.openStream();
-			 br = new BufferedReader(new InputStreamReader(is, "UTF-8"));
-			    while (( line = br.readLine()) != null) {
-		            sb.append(line);
-		        }
-		} catch (Exception e) {
-			System.out.println("URL is not valid");
-			return;
-		}
-		FileEditor.write(fileName,sb.toString());
+        }
 
-	}
-	
-	public static String getInstructions(){
-		return "Usage: some web address\n" + 
-				"      or -i\n" +
-				"Example: http://google.com";
-	}
+        executeCommand(choise);
+    }
+
+    private static void executeCommand(int commandNumber) {
+        switch (commandNumber) {
+            case 1:
+                System.out.println("Page will be saved to file: \n" + FILE_NAME);
+                savePageToFile(stringUrl, FILE_NAME);
+                break;
+
+            case 2:
+                System.out.print(getTitle(getHTMLFromURL(stringUrl)));
+                break;
+            case 3:
+                System.out.print("Type some word to find");
+                Scanner in = new Scanner(System.in);
+                try {
+                    String word = in.nextLine();
+                    System.out.print(findWordInText(getHTMLFromURL(stringUrl), word));
+                } catch (InputMismatchException e) {
+                    System.out.print("Incorrect value.");
+                    return;
+                }
+
+
+                break;
+            case 4:
+                System.out.print(getAllLinksFromText(getHTMLFromURL(stringUrl)));
+                ;
+
+
+                break;
+            case 5:
+                System.out.println("Exit");
+                break;
+            default:
+                System.out.println("Incorrect value, Exit");
+                break;
+        }
+    }
+
+    private static String getAllLinksFromText(String html) {
+        String line = "";
+        if (!html.contains("<a")) {
+            return "Can't find any URLs";
+        }
+        StringBuilder sb = new StringBuilder();
+        while (html.contains("<a href=")) {
+            Integer startIndex = html.indexOf("http");
+            Integer stopIndex = html.indexOf("\"", html.indexOf("http"));
+
+            line = html.substring(startIndex, stopIndex);
+            sb.append(line);
+            sb.append("\n");
+            html = html.substring(stopIndex);
+        }
+
+
+        return sb.toString();
+    }
+
+    private static String findWordInText(String html, String word) {
+        if (html.contains(word)) {
+            Integer startIndex = 10;
+            Integer stopIndex = 10;
+            if (html.indexOf(word) < 10) {
+                startIndex = html.indexOf(word);
+            }
+            if (((html.indexOf(word) + 10) > html.length())) {
+                stopIndex = (html.length() - html.indexOf(word));
+            }
+            html = html.substring((html.indexOf(word)) - startIndex, (html.indexOf(word)) + stopIndex);
+        } else {
+            return "The word \"" + word + "\" not exist";
+        }
+        return html;
+    }
+
+    private static String getTitle(String html) {
+        try {
+            html = html.substring((html.indexOf("<title>")) + 7, html.indexOf("</title>"));
+        } catch (StringIndexOutOfBoundsException e) {
+            return ("Cannot parse title from page.");
+
+        }
+        System.out.println("Title is : ");
+        return html;
+    }
+
+    public static void savePageToFile(String stringUrl, String fileName) {
+        FileEditor.write(fileName, getHTMLFromURL(stringUrl));
+    }
+
+    public static String getHTMLFromURL(String stringUrl) {
+        String line;
+        StringBuffer sb = new StringBuffer();
+        try {
+            URL url = new URL(stringUrl);
+            is = url.openStream();
+            br = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+                sb.append("\n");
+            }
+        } catch (Exception e) {
+            return "URL is not valid";
+        }
+        return sb.toString();
+
+
+    }
+
+    public static String getInstructions() {
+        return "Usage: some web address\n" +
+                "      or -i\n" +
+                "Example: http://google.com";
+    }
 
 }
